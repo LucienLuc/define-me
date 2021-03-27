8bfe46a7784bcf2b5bd3f31a0299f31102ada4d7
LucienLuc <lucienluc@umail.ucsb.edu>
2021-03-27 12:14:48 -0700

    reorganize file structure

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const cors = require("cors")({origin: true});
+const vision = require('@google-cloud/vision');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,6 +8,15 @@ const cors = require("cors")({origin: true});
 exports.helloWorld = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
     functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+  });
+});
+
+exports.getTerms= functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    const client = new vision.ImageAnnotatorClient();
+
+    functions.logger.info("Making vision request!", {structuredData: true});
     response.send("Hello from Firebase!");
   });
 });
